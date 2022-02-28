c1d446eecc11a0c06bc59c09c2e77c3a6dbaabc9
jin <jin8d19@gmail.com>
2022-03-01 01:06:25 +0900

    lombok 설치 및 테스트 코드 완료

--- src/main/java/com/bs/book/springboot/web/HelloController.java
+++ src/main/java/com/bs/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.bs.book.springboot.web;
 
+import dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,10 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+    //HelloController 코드를 롬복으로 전환
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 }
+
