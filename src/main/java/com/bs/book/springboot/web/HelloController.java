681b1ea182a36dea57d97fa3575deccd1ce71b4c
jin <jin8d19@gmail.com>
2022-03-08 03:08:43 +0900

    등록,수정 API 완료

--- src/main/java/com/bs/book/springboot/web/HelloController.java
+++ src/main/java/com/bs/book/springboot/web/HelloController.java
@@ -1,6 +1,6 @@
 package com.bs.book.springboot.web;
 
-import dto.HelloResponseDto;
+import com.bs.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
